bb7a949f4d87f739aa0c35272738319ff975c287
Jean Pierre Velez <vcjpierre@gmail.com>
2019-07-30 19:14:22 -0400

    webpack config

---
+++ webpack/webpack.common.js
@@ -0,0 +1,25 @@
+const path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: {
+        main: "./src/assets/javascript/entry.js",
+    },
+    module: {
+        rules: [{
+                test: /\.html$/,
+                use: ["html-loader"]
+            },
+            {
+                test: /\.(svg|png|jpg|gif)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: "[name].[hash].[ext]",
+                        outputPath: "images"
+                    }
+                }
+            }
+        ]
+    }
+};
---
+++ webpack/webpack.dev.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const common = require("./webpack.common");
+const merge = require("webpack-merge");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = merge(common, {
+    mode: "development",
+    devtool: 'source-map',
+    devServer: {
+        port: 4000,
+        open: true
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.template.html"
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                "style-loader", //3. Inject styles into DOM
+                "css-loader", //2. Turns css into commonjs
+                "sass-loader" //1. Turns sass into css
+            ]
+        }]
+    }
+});
